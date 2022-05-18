cfd4402cea558d0fd00271598343ef73695f3c9c
Kerminator-dev <pussy_hunter@list.ru>
2022-05-18 10:36:29 +0300

    Создан проект
    Создан проект
    Созданы модели

---
+++ src/Reservoom/Reservoom/Models/Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Hotel
+    {
+        private readonly ReservationBook _reservationBook;
+        public string Name { get; }
+        public Hotel(string name)
+        {
+            Name = name;
+            _reservationBook = new ReservationBook();
+        }
+
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return _reservationBook.GetReservationsForUser(username);
+        }
+
+        public void MakeReservation(Reservation reservation)
+        {
+            _reservationBook.AddReservation(reservation);
+        }
+    }
+}
---
+++ src/Reservoom/Reservoom/Models/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Reservation
+    {
+        public RoomID RoomID { get; }
+        public string Username { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+
+        public TimeSpan Length => EndTime - StartTime;
+
+        public Reservation(RoomID roomID, string usernmae, DateTime startTime, DateTime endTime)
+        {
+            RoomID = roomID;
+            Username = usernmae;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        internal bool Conflicts(Reservation reservation)
+        {
+            if (reservation.RoomID != RoomID)
+                return true;
+
+            return reservation.StartTime > EndTime;
+        }
+    }
+}
